824ac94f17609ab29da218d6cde26e7de81f7143
Peter-Schay <54046004+Peter-Schay@users.noreply.github.com>
2019-11-20 14:14:23 -0800

    add bigfile copy command and handle remanders

--- bigfile.py
+++ bigfile.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2019 NetApp Inc. - All Rights Reserved
 # This sample code is provided AS IS, with no support or warranties of any kind, including but not limited to warranties of merchantability or fitness of any kind, expressed or implied.
 #
-# Utility for Neto
+# Utility for Neto and Randy
 # Use multiple processes to work on a single big file
 
 # Updates: 
@@ -10,19 +10,25 @@
 # USAGE
 # The script dynamically adds a new "bigfile" command; run it as follows:
 # xcp diag -run bigfile.py help bigfile
-# xcp diag -run bigfile.py bigfile [options]
+# xcp diag -run bigfile.py bigfile [options] source-file-path
+# xcp diag -run bigfile.py bigfile copy [options] source-file-path target-path
+# Target-path could be a file or a directory (in which to create or replace a file)
 
 import os
+import rd
 import xcp
 import scan
+import nfs3
 import sched
+import serve
+import client
 import command
 import parseargs as args
 from basics import formatSize as fmts
 
 maxPendOption = args.OptionInfo('-maxpend', 'readahead limit', args.Types.Int, arg='# of requests', default=8)
 
-Stats = ['blocks',]
+Stats = ['blocks', 'writes']
 
 # xcp diag -run bigfile.py will run us here
 # A quirk of running it this way is that our log file will be /opt/NetApp/xFiles/xcp/xcp.x1.log
@@ -32,9 +38,22 @@ def run(argv):
 class RunBigfile(command.Runner):
 	def gRun(self, cmd, catalog):
 		f = cmd.source.root
-		nproc = cmd.options.get('parallel')
 		bs = cmd.options.get('bs')
-		print('f {}'.format(f))
+		nproc = cmd.options.get('parallel')
+
+		print('source: {}'.format(f))
+		assert f.a.type == nfs3.REG, 'source type must be regular file'
+		assert f.a.size > nproc*bs, 'source file is too small ({})'.format(fmts(f.a.size))
+
+		if cmd.desc == copyDesc:
+			print('target: {}'.format(cmd.target.root))
+			if cmd.target.root.a.type == nfs3.REG:
+				print('using existing target file {}'.format(cmd.target.root))
+				f.copy = cmd.target.root
+			else:
+				print('creating target file {}/{}'.format(cmd.target, f.name))
+				yield (rd.CreateCopyTask(f, cmd.target.root, f.name), None)
+
 		chunk = f.a.size / self.engine.parallel
 		chunk = bs*(chunk/bs)
 		b2c = chunk/bs
@@ -43,10 +62,8 @@ class RunBigfile(command.Runner):
 		))
 		print('workers x blocks = {}, readahead {}'.format(nproc * b2c, self.options.get(maxPendOption)))
 		remainder = f.a.size - chunk*(f.a.size/chunk)
-		if (remainder):
-			self.log.warn('not processing remainder {} bytes'.format(remainder), out=True)
 
-		# Get our custom stats on the console updates
+		# Get our custom stats to display on on the console
 		sched.engine.statsTask.addStats(Stats)
 
 		offset = 0
@@ -55,8 +72,17 @@ class RunBigfile(command.Runner):
 			workers.append(Worker(f, offset, bs, b2c))
 			offset += bs*b2c
 
+		if remainder:
+			print('Adding an extra worker to process remainder of {} blocks + {} bytes'.format(
+				remainder/bs, remainder-bs*(remainder/bs)))
+			workers.append(Worker(f, offset, bs, remainder/bs, remainder=remainder-bs*(remainder/bs)))
+
 		# The yield makes this task (instance of RunBigFile) wait for all the workers to finish
 		yield (workers, None)
+
+		if cmd.desc == copyDesc:
+			# We don't really need to commit with ONTAP; just doing it in case linux is the target
+			yield (f.copy.commit(), None)
 
 		print('Workers complete.  Processed {} blocks'.format(sched.engine.stats['blocks']))
 
@@ -65,51 +91,78 @@ class Worker(sched.Task):
 	# In the parent process this Worker task instance just waits on a queue and never enters the gRun() code below
 	# The child process creates a new engine which runs a copy of the Worker, which enters its gRun and does the actual IO
 	# Each child engine will open its own NFS TCP connections for f
-	def __init__(self, f, offset, bs, n, process=True):
-		g = self.gRun(f, offset, bs, n)
-		super(Worker, self).__init__(f, offset, bs, n, producer=g, process=process)
+	def __init__(self, f, offset, bs, n, remainder=None, process=True):
+		g = self.gRun(f, offset, bs, n, remainder)
+		super(Worker, self).__init__(f, offset, bs, n, remainder, producer=g, process=process)
 
 	# Read n blocks of size bs starting at offset
-	def gRun(self, f, offset, bs, n):
+	def gRun(self, f, offset, bs, n, remainder):
 		end = offset + bs*n
-		self.log.log('hello from worker {} from offset {}'.format(os.getpid(), offset), out=True)
+		self.log.log('started worker {} offset {} n {} remainder {}'.format(os.getpid(), offset, n, remainder))
 		# The gate is like a semaphore to throttle reads
 		gate = sched.Gate(self.options.get(maxPendOption), 'readahead limit')
+		# If we are copying, create a writeGate which each read task will pass to a write task
+		if f.copy:
+			writeGate = sched.Gate(self.options.get(maxPendOption), 'pending write limit')
+		else:
+			writeGate = None
 
 		while offset < end:
 			# Engine gates us here when maximum IO's are pending
 			yield (gate, None)
 
 			# Create the task; sched's global engine automatically puts it on the runq
-			Read1(f, offset, bs, gate)
+			Read1(f, offset, bs, gate, writeGate=writeGate)
 
 			offset += bs
 
 			sched.engine.stats['blocks'] += 1
 
+		if remainder:
+			yield (gate, None)
+			Read1(f, offset, remainder, gate, writeGate=writeGate)
+
 		# Wait for any pending Read1 tasks to finish
 		if not gate.close():
 			yield
 
+		if writeGate and not writeGate.close():
+			yield
+
+class Write1(sched.SimpleTask):
+	def gRun(self, f, offset, data, writeGate):
+		# All writes are stable with ONTAP; just using UNSTABLE mode in case the target is non-ONTAP
+		yield (f.write(offset, data, stable=nfs3.Stable_mode.UNSTABLE), None)
+		sched.engine.stats['writes'] += 1
+		writeGate.leave()
+
 class Read1(sched.SimpleTask):
-	def gRun(self, f, offset, bs, gate):
-		call = (yield (f.read(offset, bs), None))
+	def gRun(self, f, offset, count, gate, writeGate=None):
+		call = (yield (f.read(offset, count), None))
 		gate.leave()
+		if writeGate:
+			yield (writeGate, None)
+			Write1(f.copy, offset, call.res.data, writeGate)
 
 options = [
 	maxPendOption,
+	client.bsizeOption,
+	sched.parallelOption,
 ]
 
-# Just grab all the copy command options for now
-# Might be useful to add more features like verification
-options.extend(scan.copyOptions)
+try:
+	options.append(serve.dataOption)
+	options.append(serve.sbsOption)
+except:
+	# Not all versions of xcp have the mkdata option so just ignore
+	pass
 
 desc = command.Desc(
-	'bigfile',
-	options,
-	'Use multiple processes to work on a single file',
-	npaths=1,
-	runner=RunBigfile,
+	'bigfile', options, 'Use multiple processes to read a single file', npaths=1, runner=RunBigfile,
+)
+
+copyDesc = command.Desc(
+	"copy", options, "Copy a giant file", npaths=2, parent=desc, runner=RunBigfile,
 )
 
 xcp.commands.append((desc, RunBigfile))
